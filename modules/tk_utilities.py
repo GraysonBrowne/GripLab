99d4b7403963608c8f4eca11b0a93cf01fb9f51e
GraysonBrowne <graysonbrowne70@gmail.com>
2025-08-21 18:12:50 -0400

    Added documentation to tk_utilities

--- modules/tk_utilities.py
+++ modules/tk_utilities.py
@@ -3,6 +3,21 @@ from logger_setup import logger
 
 class Tk_utils:
     def select_file(filetypes,initialdir,icon=''):
+        """
+        Opens a file selection dialog for the user to choose a file.
+
+        Args:
+            filetypes (list of tuple): List of (label, pattern) tuples specifying allowed file types, e.g., [("Text files", "*.txt")].
+            initialdir (str): The initial directory that the dialog opens in.
+            icon (str, optional):  Path to the icon file for the dialog window. Defaults to ''.
+
+        Returns:
+            str: The path to the selected file, or an empty string if no file was selected.
+
+        Logs:
+            Logs the selected file path or a message if no file was selected.
+            Logs an error message if an exception occurs during the selection process.
+        """
         try:
             root = Tk()
             root.iconbitmap(icon)
@@ -23,6 +38,20 @@ class Tk_utils:
             logger.error(f"Error selecting file: {e}")
 
     def select_dir(initialdir,icon=''):
+        """
+        Opens a directory selection dialog and returns the selected directory path.
+
+        Args:
+            initialdir (str): The initial directory that the dialog opens in.
+            icon (str, optional): Path to the icon file for the dialog window. Defaults to ''.
+
+        Returns:
+            str: The path of the selected directory, or an empty string if no directory was selected.
+
+        Logs:
+            Logs the selected directory path or a message if no directory was selected.
+            Logs an error message if an exception occurs during the selection process.
+        """
         try:
             root = Tk()
             root.iconbitmap(icon)
@@ -42,6 +71,20 @@ class Tk_utils:
             logger.error(f"Error selecting directory: {e}")
 
     def save_file(defaultextension,initialdir,icon=''):
+        """
+        Opens a file save dialog using Tkinter and returns the selected file path.
+
+        Args:
+            defaultextension (str): The default file extension to use in the save dialog (e.g., '.txt').
+            initialdir (str): The initial directory that the dialog opens in.
+            icon (str, optional): Path to the icon file for the Tkinter window. Defaults to ''.
+
+        Returns:
+            str: The file path selected by the user, or an empty string if the operation is cancelled.
+
+        Logs:
+            Logs the selected file path or cancellation, and logs errors if any occur during the process.
+        """
         try:
             root = Tk()
             root.iconbitmap(icon)
