ca9fb687e0ab8d74c2a9a328d777186241b4a13b
GraysonBrowne <graysonbrowne70@gmail.com>
2026-04-03 10:15:36 -0400

    Add splash image when launching from executable

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # main.py
 """GripLab - Tire Data Analysis Application"""
 
+import importlib
+import os
 import sys
 from pathlib import Path
 
@@ -8,6 +10,11 @@ from pathlib import Path
 sys.path.insert(0, str(Path(__file__).parent))
 
 from app.app import GripLabApp
+
+if'_PYI_SPLASH_IPC' in os.environ and importlib.uyit.find_spec("pyi_splash"):
+    import pyi_splash
+    pyi_splash.update_text("UI Loaded ...")
+    pyi_splash.close()
 
 
 def main():
