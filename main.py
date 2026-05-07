45da2d57f04c714ff21ccb9d8dac28926807a67a
GraysonBrowne <graysonbrowne70@gmail.com>
2026-05-06 22:26:13 -0400

    Show/close splash image

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 # main.py
 """GripLab - Tire Data Analysis Application"""
 
+import importlib
+import os
+
 from app.app import GripLabApp
 
+if'_PYI_SPLASH_IPC' in os.environ and importlib.util.find_spec("pyi_splash"):
+    import pyi_splash
+    pyi_splash.update_text("UI Loaded ...")
+    pyi_splash.close()
 
 def main():
     """Main entry point."""
